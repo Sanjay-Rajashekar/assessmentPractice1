767edd71443132a46976848f1143f364c78b4b0b
Sanjay Rajashekar <sanjay.rajashekar@publicissapient.com>
2022-11-30 08:06:44 +0530

    files added

---
+++ src/test/java/concat/concatination.java
@@ -0,0 +1,14 @@
+package concat;
+
+import org.testng.annotations.Test;
+
+public class concatination {
+	
+	@Test
+	public void concat() {
+		String firstname="Sanjay";
+		String lastname = "Rajashekar";
+		System.out.println("Hello "+firstname + " "+lastname);
+	}
+
+}
